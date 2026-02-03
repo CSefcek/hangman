0f44d23b6c239cb41627d145cdb1919a1daf89c1
CSefcek <89878008+CSefcek@users.noreply.github.com>
2026-02-03 13:48:36 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿Console.Title = "Hangman";
+Console.WriteLine("Welcome to Hangman!");
+
+string[] hangman = new string[8]
+{
+    """
+    +---+
+           |
+           |
+           |
+           |
+           |
+    ======= 
+    """,
+    """
+    +---+
+      |    |
+           |
+           |
+           |
+           |
+    =======
+    """,
+    """
+    +---+
+      |    |
+      O    |
+           |
+           |
+           |
+    =======
+    """,
+    """
+    +---+
+      |    |
+      O    |
+     /     |
+           |
+           |
+    =======
+    """,
+    """
+    +---+
+      |    |
+      O    |
+     /|    |
+           |
+           |
+    =======
+    """,
+    """
+    +---+
+      |    |
+      O    |
+     /|\   |
+           |
+           |
+    =======
+    """,
+    """
+    +---+
+      |    |
+      O    |
+     /|\   |
+     /     |
+           |
+    =======
+    """,
+    """
+    +---+
+      |    |
+      O    |
+     /|\   |
+     / \   |
+           |
+    =======
+    """
+
+};
+
+string[] words = new string[10] 
+{
+    "kernel",
+    "classes", 
+    "console", 
+    "coffee", 
+    "arrays", 
+    "recursion", 
+    "boolean", 
+    "conditional", 
+    "loops", 
+    "methods"
+};
+
+Random random = new Random();
+string wordToBeGuessed = words[random.Next(words.Length)];
+int wordLength = wordToBeGuessed.Length;
+char[] wordToBeGuessedArray = wordToBeGuessed.ToCharArray();
+
+
+string blanks = new string('_', wordLength);
+Console.WriteLine(blanks);
+
+
+while (true)
+{
+    Console.WriteLine("Guess a letter!");
+    char guess = Convert.ToChar(Console.ReadLine());
+
+    foreach (char character in wordToBeGuessedArray)
+    {
+        Console.WriteLine(character);
+
+    }
+
+    //if (wordToBeGuessed.IndexOf(guess) != 0)
+    //{
+
+    //}
+}
